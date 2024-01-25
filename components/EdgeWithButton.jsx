4a986537dfa771228798af3ef91a1498f475afb0
Sheikh Ariful Islam <sheikharifulislam101@gmail.com>
2024-01-25 06:35:17 +0600

    feat: have added custom edge for add button on the edge middle

---
+++ components/EdgeWithButton.jsx
@@ -0,0 +1,48 @@
+import { BaseEdge, EdgeLabelRenderer, getStraightPath } from "reactflow";
+
+const EdgeWithButton = ({ id, sourceX, sourceY, targetX, targetY }) => {
+    const [path, labelX, labelY, offsetX, offsetY] = getStraightPath({
+        sourceX,
+        sourceY,
+        targetX,
+        targetY,
+    });
+    return (
+        <>
+            <BaseEdge id={id} path={path} />
+            <EdgeLabelRenderer>
+                <button
+                    style={{
+                        position: "absolute",
+                        transform: `translate(-50%, -50%) translate(${labelX}px,${labelY}px)`,
+                        pointerEvents: "none",
+                        border: "1px solid #eee",
+                        padding: "4px 10px",
+                        fontSize: "14px",
+                        background: "#fff",
+                        borderRadius: "4px",
+                    }}
+                >
+                    <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 18 18" fill="none">
+                        <path
+                            d="M4.5 8.99998H13.5"
+                            stroke="#2F9089"
+                            strokeWidth="1.25"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        />
+                        <path
+                            d="M9 4.49998V13.5"
+                            stroke="#2F9089"
+                            strokeWidth="1.25"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                        />
+                    </svg>
+                </button>
+            </EdgeLabelRenderer>
+        </>
+    );
+};
+
+export default EdgeWithButton;
