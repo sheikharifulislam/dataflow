4243a9e55f94a8a447ea3cda0f4a9b6ef0c3ba57
Sheikh Ariful Islam <sheikharifulislam101@gmail.com>
2024-01-25 06:32:08 +0600

    feat: have added initial dat

---
+++ data/index.js
@@ -0,0 +1,33 @@
+const data = [
+    {
+        title: "Tree Start",
+    },
+    {
+        title: "Step 1",
+    },
+    {
+        title: "Step 2",
+        child: [
+            {
+                title: "Sub Tree",
+                curve: true,
+                child: [
+                    {
+                        title: "Step 1",
+                    },
+                ],
+            },
+            {
+                title: "Sub Tree",
+                curve: true,
+                child: [
+                    {
+                        title: "Step 1",
+                    },
+                ],
+            },
+        ],
+    },
+];
+
+export default data;
